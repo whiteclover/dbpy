9720fb51339ff556a25a233c99265c828d213df3
thomashuang <lyanghwy@gmail.com>
2015-07-26 13:16:38 +0800

    tag: version 0.1.2

--- db/__init__.py
+++ db/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 VERSION = tuple(map(int, __version__.split('.')))
 
 
